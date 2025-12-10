3254f50322ba71a8e2a6cfaf59eb8a8ff4bbda7b
Emer Yu <emeryu@Emer-Yu.local>
2025-12-10 10:23:34 +0800

    將資料庫excel輸入但是streamlit頁面還是空白無法使用

--- Pet_NutriTracker_page/pages/2_daily_record_page.py
+++ Pet_NutriTracker_page/pages/2_daily_record_page.py
@@ -5,14 +5,12 @@ import pandas as pd
 st.title("每日紀錄")
 
 #讀取 Excel 檔案
-excel_path = "final_nutrition_data.csv"
-#哪一個sheet
-sheet_name = "工作表 1"
+csv_path = "final_nutrition_data.csv"
 
 try:
-    df = pd.read_excel(excel_path, sheet_name=sheet_name)
+    df = pd.read_csv(csv_path)
 except FileNotFoundError:
-    st.error(f"找不到 Excel 檔案：{excel_path}")
+    st.error(f"找不到 Excel 檔案：{csv_path}")
     st.stop()
 except Exception as e:
     st.error(f"讀取 Excel 發生錯誤：{e}")
