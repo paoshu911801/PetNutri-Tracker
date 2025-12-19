05e3c22bf1d0959fce81ffcbcaea43abd1f0a6a1
Emer Yu <emeryu@Emer-Yu.local>
2025-12-19 11:30:50 +0800

    每日紀錄頁面更新，但仍無法在輸入重量後計算熱量及蛋白質

--- Pet_NutriTracker_page/pages/2_daily_record_page.py
+++ Pet_NutriTracker_page/pages/2_daily_record_page.py
@@ -5,7 +5,7 @@ import pandas as pd
 st.title("每日紀錄")
 
 #讀取 Excel 檔案
-csv_path = "../final_nutrition_data.csv"
+csv_path = "final_nutrition_data.csv"
 
 try:
     df = pd.read_csv(csv_path)
@@ -15,24 +15,41 @@ except FileNotFoundError:
 except Exception as e:
     st.error(f"讀取 Excel 發生錯誤：{e}")
     st.stop()
-st.write("目前欄位名稱：", df.columns.tolist())
 
-
-# === 2. 清理資料（補 0、移除空白食物）===
+# 清理資料（補 0）
 #df = df.dropna(subset=["品項"])
 df = df.fillna(0)
-st.write("df shape：", df.shape)
-st.dataframe(df)
 
-# === 3. UI ===
+# UI
 st.subheader("請輸入紀錄")
-food_option = st.selectbox("選擇食物", df["品項"])
-weight = st.number_input("請輸入重量 (g)", min_value=0.0, step=10.0)
+#food_brand_option = df["品牌"].astype(str).str.strip()
+#food_brand_option = food_brand_option[(food_brand_option != "") & (food_brand_option != "0") & (food_brand_option !="-")]
+#food_brand_option = sorted(food_brand_option.unique()) #去掉重複的值
+#food_brand_option = st.selectbox("食物的品牌", food_brand_option)
+food_option = df["品項"].astype(str).str.strip()
+food_option = food_option[(food_option !="") & (food_option !="0") & (food_option !="-")]
+food_option = sorted(food_option.unique()) #去掉重複的值
+food_option = st.selectbox("食物品項", food_option)
+weight = st.number_input("請輸入重量 (g)", min_value=0.0, step=1.0)
 
-# === 4. 計算與輸出 ===
-selected_row = df[df["品項"] == food_option].iloc[0]
-kcal = selected_row.get("熱量 (kcal/100g)", 0) * weight / 100
-protein = selected_row.get("蛋白質 (g/100g)", 0) * weight / 100
+# 計算與輸出
+cleaned_food_option = str(food_option).strip()
+matched_rows = df[df["品項"].astype(str).str.strip() == cleaned_food_option]
+st.write("比對結果：", matched_rows)
+if not matched_rows.empty:
+    selected_row = matched_rows.iloc[0]
+    try:
+        kcal = float(selected_row["熱量 (kcal/100g)"]) * weight / 100
+    except (KeyError, ValueError, TypeError):
+        kcal = 0.0
+
+    try:
+        protein = float(selected_row["蛋白質 (g/100g)"]) * weight / 100
+    except (KeyError, ValueError, TypeError):
+        protein = 0.0
+else:
+    kcal = 0.0
+    protein = 0.0
 
 st.subheader("計算結果")
 st.write(f"熱量：{kcal:.2f} kcal")
